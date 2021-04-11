1101554a05992232dca9a48bb89aaf3aaf96a277
Andres Chapeton <andres.chapeton0206@gmail.com>
2021-04-11 17:22:11 -0600

    Modulo Registro

--- Parcial_02/app/src/main/java/sv/edu/udb/parciallab2/SignUp.java
+++ Parcial_02/app/src/main/java/sv/edu/udb/parciallab2/SignUp.java
@@ -8,7 +8,6 @@ import android.app.ProgressDialog;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-import android.widget.EditText;
 import android.widget.Toast;
 
 import com.google.firebase.database.DataSnapshot;
@@ -18,45 +17,46 @@ import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 import com.rengwuxian.materialedittext.MaterialEditText;
 
-public class SignIn extends AppCompatActivity {
-    EditText edtPhone,edtPassword;
-    Button btnSignIn;
+public class SignUp extends AppCompatActivity {
+
+    MaterialEditText edtPhone, edtName, edtPassword;
+    Button btnSignUp;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_sign_in);
+        setContentView(R.layout.activity_sign_up);
 
+        edtName = (MaterialEditText)findViewById(R.id.edtName);
         edtPhone = (MaterialEditText)findViewById(R.id.edtPhone);
         edtPassword = (MaterialEditText)findViewById(R.id.edtPassword);
+
+        btnSignUp = (Button)findViewById(R.id.btnSignUp);
 
         final FirebaseDatabase database = FirebaseDatabase.getInstance();
         final DatabaseReference table_user = database.getReference("User");
 
-        btnSignIn.setOnClickListener(new View.OnClickListener() {
+        btnSignUp.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                ProgressDialog mDialog = new ProgressDialog(SignIn.this);
+                ProgressDialog mDialog = new ProgressDialog(SignUp.this);
                 mDialog.setMessage("Espere un momento...");
                 mDialog.show();
 
                 table_user.addValueEventListener(new ValueEventListener() {
                     @Override
                     public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
-                        //Check user
                         if(dataSnapshot.child(edtPhone.getText().toString()).exists()){
-                            //Get user
                             mDialog.dismiss();
-                            User user = dataSnapshot.child(edtPhone.getText().toString()).getValue(User.class);
-                            if(user.getPassword().equals(edtPassword.getText().toString())){
-                                Toast.makeText(SignIn.this, "Inició sesión correctamente", Toast.LENGTH_SHORT).show();
-                            }else{
-                                Toast.makeText(SignIn.this, "Datos incorrectos", Toast.LENGTH_SHORT).show();
-                            }
+                            Toast.makeText(SignUp.this, "Número de teléfono ya registrado", Toast.LENGTH_SHORT).show();;
                         }
                         else{
                             mDialog.dismiss();
-                            Toast.makeText(SignIn.this, "Usuario no registrado", Toast.LENGTH_SHORT).show();
+                            User user = new User(edtName.getText().toString(),edtPassword.getText().toString());
+                            table_user.child(edtPhone.getText().toString()).setValue(user);
+                            Toast.makeText(SignUp.this, "Registro exitoso", Toast.LENGTH_SHORT).show();
+                            finish();
+
                         }
                     }
 
@@ -67,6 +67,5 @@ public class SignIn extends AppCompatActivity {
                 });
             }
         });
-
     }
 }
